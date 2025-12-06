fbcbc00b6ae1db99eb073a14054ec390d04104a9
SkyMonder <rilloperdagelo@gmail.com>
2025-12-06 18:50:39 +0300

    Update SkyMail.py

--- SkyMail.py
+++ SkyMail.py
@@ -31,7 +31,7 @@ def allowed_file(filename):
 SMTP_SERVER = "smtp.gmail.com"  # или smtp.mail.ru
 SMTP_PORT = 465
 SMTP_USER = "skymonder@yandex.ru"  # ваш email
-SMTP_PASS = "harapjyzlsnsnugo"     # пароль приложения Gmail
+SMTP_PASS = "xdbattimavtxfggf"     # пароль приложения Gmail
 
 def send_external_email(to_email, subject, body, attachments=[]):
     try:
@@ -252,4 +252,5 @@ def logout():
 if __name__=="__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+
 
